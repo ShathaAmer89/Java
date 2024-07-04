f0c06525f574f8bdb43303f195c2d4d0799ff585
shatha amer <shatha.amer89@gmail.com>
2024-07-04 15:34:46 +0200

    Hvordan kan jeg definere variabler

---
+++ src/Variabler/Strenger.java
@@ -0,0 +1,12 @@
+package Variabler;
+
+public class Strenger {
+    public static void main(String[] args) {
+        String navn="shatha";
+        String alder="31";
+        String ut=navn+" " +alder;
+        System.out.println(ut);
+
+
+    }
+}
